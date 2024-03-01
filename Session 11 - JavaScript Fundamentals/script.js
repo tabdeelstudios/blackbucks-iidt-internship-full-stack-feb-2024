d07d04d1168aa0216863878678536668ec2bfe84
Aquib Ajani <aquib@tabdeel.in>
2024-03-01 13:28:19 +0530

    Add files via upload

---
+++ Session 11 - JavaScript Fundamentals/script.js
@@ -0,0 +1,28 @@
+// Number
+var salary = 25000.00;
+var age = 30;
+var count = 0;
+var profit = -50000
+// String
+var username = 'john'
+var agree = 'y'
+var password = "123^#%$!"
+// Boolean
+let isStudent = true
+isFaculty = false
+// Object
+profile={
+    username:"hello123",
+    age:54,
+    mobile:"84747474774",
+    email:"random@hello.com"
+}
+console.log(profile)
+
+let x = 10;
+{
+    const x = 20;
+    x = 30;
+    console.log(x);
+}
+console.log(x);
---
+++ Session 12 - Intermediate Javascript/scripts.js
@@ -0,0 +1,41 @@
+// HTML Manipulation
+// let headings = document.getElementsByTagName("h1");
+
+// heading.innerText = "New content";
+
+// CSS Manipulation
+// heading.style.textDecoration = "underline";
+// heading.style.color = "red";
+
+// console.log(heading.innerHTML);
+// console.log(headings);
+
+// function changeBG() {
+//   headings[2].style.backgroundColor = "yellow";
+// }
+
+// function changeBG1() {
+//   headings[0].style.backgroundColor = "pink";
+// }
+
+// function resetBG() {
+//   headings[2].style.backgroundColor = "transparent";
+// }
+
+// let buttons = document.getElementsByTagName("button");
+
+// console.log(buttons);
+
+// let body = document.getElementsByTagName("body");
+
+// function changeBackground() {
+//   body[0].style.backgroundColor = "pink";
+// }
+
+// function resetBackground() {
+//   body[0].style.backgroundColor = "transparent";
+// }
+
+window.alert("This is important!");
+username = window.prompt("What is your name?");
+console.log(username);
