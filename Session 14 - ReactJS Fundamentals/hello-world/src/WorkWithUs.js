a8165891ef86f1c93fd717dd9d2fd74b90cd0f5d
Aquib Ajani <aquib@tabdeel.in>
2024-03-06 17:59:26 +0530

    Add files via upload

---
+++ Session 14 - ReactJS Fundamentals/hello-world/src/WorkWithUs.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function WorkWithUs() {
+  //logic will come here
+
+  return <h1>Work with us</h1>;
+  //JSX - Javascript eXpressions
+}
+
+export default WorkWithUs;
---
+++ Session 14 - ReactJS Fundamentals/hello-world/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import WorkWithUs from "./WorkWithUs";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<WorkWithUs />);
